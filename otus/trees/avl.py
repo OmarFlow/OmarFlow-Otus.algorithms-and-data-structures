994e1a3edd6d88077c1ae784e5019dbeec5b6c6a
Roman <r.serov@idaproject.com>
2022-08-07 14:03:08 +0300

    bst/avl

---
+++ otus/trees/avl.py
@@ -0,0 +1,160 @@
+from otus.trees.bst import BST
+
+
+class AVL(BST):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def insert(self, key):
+        if self.is_root:
+            self._insert(key)
+        else:
+            root = self.move_to_root()
+            root._insert(key)
+
+    def _insert(self, key):
+        if key == self.key:
+            self.values.append(key)
+            return
+
+        if key > self.key:
+            if self.right is None:
+                self.right = AVL(key, self)
+                self.find_bad_balance()
+                return
+            else:
+                self.right._insert(key)
+
+        if key < self.key:
+            if self.left is None:
+                self.left = AVL(key, self)
+                self.find_bad_balance()
+                return
+            else:
+                self.left._insert(key)
+
+    def search(self, key):
+        if self.is_root:
+            return self._search(key)
+        else:
+            root = self.move_to_root()
+            return root._search(key)
+
+    def _search(self, key):
+        if key == self.key:
+            return self
+
+        if key > self.key:
+            if not self.right:
+                return None
+            return self.right._search(key)
+
+        if key < self.key:
+            if not self.left:
+                return None
+            return self.left._search(key)
+
+    def find_bad_balance(self):
+        self.calculate_height()
+
+        if self.parent is None:
+            return
+
+        if self.need_rebalance:
+            self.rebalance()
+
+        if self.parent is None:
+            return
+
+        self.parent.find_bad_balance()
+
+    def rebalance(self):
+        another_side_height = self.get_another_side_height()
+        left_children_height = 0 if not self.left else self.left.height
+        right_children_height = 0 if not self.right else self.right.height
+
+        if self.height > another_side_height and self.parent_side == "left":
+            if left_children_height >= right_children_height:
+                self.small_right_rotation()
+            elif left_children_height < right_children_height:
+                self.big_right_rotation()
+
+        elif self.height > another_side_height and self.parent_side == "right":
+            if right_children_height >= left_children_height:
+                self.small_left_rotation()
+            elif right_children_height < left_children_height:
+                self.big_left_rotation()
+
+    def big_right_rotation(self):
+        self.right.small_left_rotation()
+        self.parent.small_right_rotation()
+
+    def big_left_rotation(self):
+        self.left.small_right_rotation()
+        self.parent.small_left_rotation()
+
+    def small_right_rotation(self):
+        if self.parent.parent:
+            parent_side = self.parent.parent_side
+
+        if self.right:
+            self.parent.left = self.right
+            self.right.parent = self.parent
+        else:
+            self.parent.left = None
+
+        self.right = self.parent
+
+        if self.parent.parent:
+            parent = self.parent
+            self.parent = self.parent.parent
+            parent.parent = self
+            setattr(self.parent, parent_side, self)
+        else:
+            self.parent.parent = self
+            self.parent = None
+        self.calculate_height()
+        self.right.calculate_height()
+
+    def small_left_rotation(self):
+        if self.parent.parent:
+            parent_side = self.parent.parent_side
+
+        if self.left:
+            self.parent.right = self.left
+            self.left.parent = self.parent
+        else:
+            self.parent.right = None
+
+        self.left = self.parent
+
+        if self.parent.parent:
+            parent = self.parent
+            self.parent = self.parent.parent
+            parent.parent = self
+            setattr(self.parent, parent_side, self)
+        else:
+            self.parent.parent = self
+            self.parent = None
+        self.left.calculate_height()
+        self.calculate_height()
+
+    @property
+    def need_rebalance(self):
+        another_side_height = self.get_another_side_height()
+        return all(False for i in [1, -1, 0] if i == self.height - another_side_height)
+
+    def get_another_side(self):
+        if self.key > self.parent.key:
+            another_side = self.parent.left
+        else:
+            another_side = self.parent.right
+        return another_side
+
+    def get_another_side_height(self):
+        another_side = self.get_another_side()
+        if another_side:
+            another_side_height = another_side.height
+        else:
+            another_side_height = 0
+        return another_side_height
---
+++ otus/trees/bst.py
@@ -0,0 +1,165 @@
+class BST:
+    def __init__(self, key, parent=None):
+        self.left = None
+        self.right = None
+        self.key = key
+        self.values = [key]
+        self.parent = parent
+        self.height = 1
+
+    def insert(self, key):
+        if key == self.key:
+            self.values.append(key)
+            return
+
+        if key > self.key:
+            if not self.right:
+                self.right = BST(key, self)
+                return
+            else:
+                self.right.insert(key)
+
+        if key < self.key:
+            if not self.left:
+                self.left = BST(key, self)
+                return
+            else:
+                self.left.insert(key)
+
+    def search(self, key):
+        if key == self.key:
+            return self
+
+        if key > self.key:
+            if not self.right:
+                return None
+            return self.right.search(key)
+
+        if key < self.key:
+            if not self.left:
+                return None
+            return self.left.search(key)
+
+    def remove(self, key):
+        root = self.move_to_root()
+        item = root.search(key)
+
+        if not item:
+            return
+
+        if item.number_of_children == 0:
+            item.remove_leaf()
+        elif item.number_of_children == 1:
+            return item.remove_one_child()
+        elif item.number_of_children == 2:
+            item.remove_two_child()
+
+    def remove_leaf(self):
+        if self.is_root:
+            self.remove_item()
+            return
+        setattr(self.parent, self.parent_side, None)
+        self.parent.calculate_height()
+        self.remove_item()
+
+    def remove_one_child(self):
+        if self.is_root:
+            node = self.left if self.left else self.right
+            setattr(self, node.parent_side, None)
+            node.parent = None
+            return node
+
+        child = self.left if self.left else self.right
+        child.parent = self.parent
+        setattr(self.parent, self.parent_side, child)
+        self.parent.calculate_height()
+        self.remove_item()
+
+    def remove_two_child(self):
+        if self.right.left is None:
+            if self.is_root:
+                self.right.parent = None
+            else:
+                self.right.parent = self.parent
+                setattr(self.parent, self.parent_side, self.right)
+            self.right.left = self.left
+            self.left.parent = self.right
+            right = self.right
+            self.remove_item()
+            return right
+        else:
+            max_left_node = self.right.move_left()
+            if max_left_node.right:
+                max_left_node.right.parent = max_left_node.parent
+                setattr(max_left_node.parent, max_left_node.parent_side, max_left_node.right)
+                self.swap_deleted_item(max_left_node)
+                max_left_node.calculate_height()
+                self.remove_item()
+                return max_left_node
+            else:
+                self.swap_deleted_item(max_left_node)
+                max_left_node.calculate_height()
+                self.remove_item()
+                return max_left_node
+
+    @staticmethod
+    def sort(node):
+        if node is None:
+            return
+        node.sort(node.left)
+        print(node.values)
+        node.sort(node.right)
+
+    @property
+    def parent_side(self):
+        if self.key > self.parent.key:
+            return "right"
+        else:
+            return "left"
+
+    @property
+    def number_of_children(self):
+        return sum(1 for i in [self.left, self.right] if i is not None)
+
+    def calculate_height(self):
+        self.height = max(self.left.height if self.left else 0, self.right.height if self.right else 0) + 1
+
+    def move_left(self):
+        left = self.left
+        while left.left:
+            left = getattr(left, "left")
+        return left
+
+    def swap_deleted_item(self, item):
+        if not item.right:
+            setattr(item.parent, item.parent_side, None)
+
+        if self.is_root:
+            item.parent = None
+        else:
+            item.parent = self.parent
+            setattr(self.parent, self.parent_side, item)
+        item.right = self.right
+        item.left = self.left
+        self.right.parent = item
+        self.left.parent = item
+
+    def remove_item(self):
+        self.parent = None
+        self.right = None
+        self.left = None
+
+    @property
+    def is_root(self):
+        if self.parent is None:
+            return True
+        return False
+
+    def move_to_root(self):
+        item = self
+        while not item.is_root:
+            item = item.parent
+        return item
+
+    def __str__(self):
+        return str(self.values)
